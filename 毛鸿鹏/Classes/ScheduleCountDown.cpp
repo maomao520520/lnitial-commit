3277c678b2eab12e836a43a3af4bc383ee6a5c09
maomao520520 <32389114+maomao520520@users.noreply.github.com>
2017-12-29 13:32:07 +0800

    毛鸿鹏211501251

---
+++ 毛鸿鹏/Classes/ScheduleCountDown.cpp
@@ -0,0 +1,48 @@
+﻿#include "ScheduleCountDown.h"
+
+USING_NS_CC;
+ScheduleCountDown* ScheduleCountDown::create(ScheduleCounterDelegate* target, int perimeter, bool loop)
+{
+	ScheduleCountDown *countDown=new ScheduleCountDown;
+	if(countDown && countDown->init(target,perimeter,loop))
+	{
+		countDown->autorelease();
+		return countDown;
+	}
+	CC_SAFE_DELETE(countDown);
+	return NULL;
+}
+
+bool ScheduleCountDown::init(ScheduleCounterDelegate* target, int perimeter, bool loop)
+{
+	if(!CCNode::init())
+	{
+		return false;
+	}
+	_target=target;
+	_maxTime =_currentTime = perimeter;
+	_loop=loop;
+
+	schedule(schedule_selector(ScheduleCountDown::schedulePerSecond),1.0f);
+	return true;
+}
+
+void ScheduleCountDown::schedulePerSecond(float delta)
+{
+	_currentTime--;
+	if(_currentTime <0)
+	{
+		_target->scheduleTimeUp();
+		if(_loop==true)
+		{
+			_currentTime =_maxTime;
+		}
+		else
+		{
+			this->unschedule(schedule_selector(ScheduleCountDown::schedulePerSecond));
+		}
+		return ;
+	}
+	else
+	_target->setScheduleNumber(_currentTime);
+}
